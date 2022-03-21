cdce562ee5886eb707fd147cb90e9a6b27546fa1
MIHIRSHARMA2000 <mihirsharma@deloitte.com>
2022-03-21 14:56:26 +0530

    mini_assignment_6

---
+++ mini_assignment_6/src/FetchRecord.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+public class FetchRecord {
+    public static void main(String[] args) throws Exception {
+//parsing a CSV file into Scanner class constructor
+        Scanner sc = new Scanner(new File("C:\\Users\\mihirsharma\\Downloads\\record.csv"));
+        sc.useDelimiter(",");   //sets the delimiter pattern
+        while (sc.hasNext())  //returns a boolean value
+        {
+            System.out.print(sc.next());  //find and returns the next complete token from this scanner
+        }
+        sc.close();  //closes the scanner
+    }
+}
